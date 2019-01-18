58e6d094e7b4a0e9727bb0adb66b127182ff370c
Edgar Gutierrez <edgargutierrez130@gmail.com>
2019-01-17 18:35:11 -0800

    learning const

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// keyword var means variable
+// name is the name of the variable
+// = mean assigning valuse to 'name' variable
+// "Edgar" is the value assigned to 'name' variable
+// TYPE string
+var name = "Edgar";
+// TYPE number
+var age = 5;
+// TYPE array
+var arr = [5,2,45, "abc", "grt"];
+// TYPE object
+var obj = {
+  firstName: 'Edgar',
+  lastName: 'Gutierrez',
+  age: 10
+};
+//
+// console.log("hi, i'm ", name);
+// var greeting = "hi, i'm " + name;
+// console.log(greeting);
+// console.log("hi, i'm ", name, age);
+console.log("hi, i'm ", obj['firstName']);
